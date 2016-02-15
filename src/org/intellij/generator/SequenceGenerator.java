85e9d461d2f24e6a17cfeaef2ace31dc0d3c09dd
Raymond <raymondcode@outlook.com>
2016-02-15 11:57:45 +0800

    finish generating call stack

--- src/org/intellij/generator/SequenceGenerator.java
+++ src/org/intellij/generator/SequenceGenerator.java
@@ -6,13 +6,17 @@ import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiMethodCallExpression;
 import org.intellij.utils.PsiUtil;
 
+import java.util.Stack;
+
 /**
  * Created by ryker.zhang on 2016/2/2.
  */
 public class SequenceGenerator extends JavaElementVisitor {
     String text = "";
+    Stack<PsiMethod> methodStack = new Stack<>();
 
     public SequenceGenerator(PsiMethod psiMethod) {
+        methodStack.push(psiMethod);
         psiMethod.accept(this);
     }
 
@@ -24,11 +28,25 @@ public class SequenceGenerator extends JavaElementVisitor {
     @Override
     public void visitMethodCallExpression(PsiMethodCallExpression psiMethodCallExpression) {
         PsiMethod psiMethod = psiMethodCallExpression.resolveMethod();
+        PsiMethod baseMethod = methodStack.peek();
+
         if (psiMethod != null) {
-            text += psiMethod.getName();
+            methodStack.push(psiMethod);
+
+            text += baseMethod.getName() + "->" + psiMethod.getName();
             text += "\n";
+
             psiMethod.accept(this);
+
+            text += baseMethod.getName() + "<-" + psiMethod.getName();
+            text += "\n";
+
+            // Make sure the initial method not deleted
+            if (methodStack.size() > 1) {
+                methodStack.pop();
+            }
         }
+
         super.visitMethodCallExpression(psiMethodCallExpression);
     }
 
