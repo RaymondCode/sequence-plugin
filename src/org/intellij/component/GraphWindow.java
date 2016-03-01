5a3daa4d3e9b32b9369b96cd342daa9f2d1e498c
Raymond <raymondcode@outlook.com>
2016-03-01 20:17:14 +0800

    support uml image

---
+++ src/org/intellij/component/GraphWindow.java
@@ -0,0 +1,39 @@
+package org.intellij.component;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by ryker.zhang on 2016/3/1.
+ */
+public class GraphWindow implements ToolWindowFactory {
+    private JPanel myToolWindowContent;
+    private ToolWindow myToolWindow;
+    private JLabel label;
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        myToolWindow = toolWindow;
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindowContent, "", false);
+        toolWindow.getContentManager().addContent(content);
+
+        BufferedImage myPicture = null;
+        try {
+            myPicture = ImageIO.read(new File("D:\\test\\test.png"));
+            label.setIcon(new ImageIcon(myPicture));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
