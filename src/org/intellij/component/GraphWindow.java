c0e5f7468c393ac81eaae8703fb11c60b7c05b90
Raymond <raymondcode@outlook.com>
2016-03-06 02:06:27 +0800

    add image render

--- src/org/intellij/component/GraphWindow.java
+++ src/org/intellij/component/GraphWindow.java
@@ -1,22 +1,27 @@
 package org.intellij.component;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import net.sourceforge.plantuml.SourceStringReader;
 import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
  * Created by ryker.zhang on 2016/3/1.
  */
 public class GraphWindow implements ToolWindowFactory {
+    private static Logger logger = Logger.getInstance(GraphWindow.class);
+
     private JPanel myToolWindowContent;
     private ToolWindow myToolWindow;
     private JLabel label;
@@ -28,12 +33,23 @@ public class GraphWindow implements ToolWindowFactory {
         Content content = contentFactory.createContent(myToolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
 
-        BufferedImage myPicture;
+        String source = "@startuml\n";
+        source += "Bob -> Alice : hello\n";
+        source += "@enduml\n";
+
+        SourceStringReader reader = new SourceStringReader(source);
+
         try {
-            myPicture = ImageIO.read(new File("D:\\test\\test.png"));
+            ByteArrayOutputStream png = new ByteArrayOutputStream();
+            reader.generateImage(png);
+
+            ByteArrayInputStream input = new ByteArrayInputStream(png.toByteArray());
+            BufferedImage myPicture = ImageIO.read(input);
             label.setIcon(new ImageIcon(myPicture));
+
+            png.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Generate uml image failed", e);
         }
     }
 }
